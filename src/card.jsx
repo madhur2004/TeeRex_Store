0d491e542624bf4fa7000d5238d2609749005017
Madhur Chaturvedi <117741881+madhur2004@users.noreply.github.com>
2025-03-05 15:05:26 +0530

    Update card.jsx

--- src/card.jsx
+++ src/card.jsx
@@ -1,14 +1,6 @@
-import { useState } from "react";
-
 export const Card = ({ product }) => {
   //console.log({ product });
   const { imageURL, id, price, name, color, gender, type } = product;
-  const [addToCart] = useState([]);
-
-  const handleAddToCart = () => {
-    addToCart(product);
-    console.log("add Ite,", addToCart(product));
-  };
 
   return (
     <>
@@ -17,8 +9,10 @@ export const Card = ({ product }) => {
           className="h-57 w-104 p-2 ml-1 mt-2 bg-gray-200 rounded-2xl  "
           id="container"
         >
+          {/* Name of Product */}
           <div>
             <h3>{name}</h3>
+            {/* Image of Product */}
             <img
               className="h-35 w-90 justify-self-center rounded-4xl"
               src={imageURL}
@@ -26,19 +20,12 @@ export const Card = ({ product }) => {
             />
           </div>
           <div className="flex justify-between items-center">
+            {/* Price of Product */}
             <div className="h-5 w-5 p-4">Rs.{price}</div>
-            {/* <button className="bg-gray-700 rounded-lg text-white p-2 mt-3 mr-8">
-          Add To Cart
-        </button> */}
+            {/* Gender & Colour of Product */}
             <span className="text-gray-500 text-sm mt-5">
               {gender} | {color}
             </span>
-            {/* <button
-              onClick={handleAddToCart}
-              className="w-30 mt-2 bg-blue-600 text-white py-2 px-1 rounded-md hover:bg-blue-700 transition-colors"
-            >
-              Add to Cart
-            </button> */}
           </div>
         </div>
       </li>
