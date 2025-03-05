26382dafad421594a7a24979803cdfe108916a38
Madhur Chaturvedi <117741881+madhur2004@users.noreply.github.com>
2025-03-05 15:03:16 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
     <>
+      {/* Footer Of the TeeRex Store */}
       <div className="bg-black  h-30 flex items-center justify-center mt-2 ">
-        <div class="  text-center text-sm text-blue-400 ">
+        <div className="  text-center text-sm text-blue-400 ">
           <p className="p-2  ">
             &copy; 2025 TeeRex T-shirt Store. All rights reserved.
           </p>
